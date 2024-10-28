666d04ae52f4d1c164ec52b15f14731f35b766dd
ZhenyaKrasilnikov <zhenyakras2707@gmail.com>
2024-10-28 15:07:05 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,10 @@
+course_name = "Python"
+
+number_of_tasks = 12
+
+hours_spent = 1.5
+print(hours_spent / number_of_tasks)
+
+task_completion_time = 0.125
+
+print("Название курса:",course_name,", " "количество задач:",number_of_tasks,", " "затрачено часов:",hours_spent,", " "время выполнения задания:",hours_spent / number_of_tasks,". ")
